618c552c2e89896ffb8007607a684d4c09bc1f2b
Sherry He <xsherryhe@gmail.com>
2023-01-12 13:20:46 -0500

    Wrote snapshot test for Home component

---
+++ src/tests/Home.test.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import './setupTests';
+import Home from '../components/Home';
+
+jest.mock('react-router-dom', () => ({
+  Link: ({ to, children }) => <a href={to}>{children}</a>,
+}));
+
+it('renders the home content', () => {
+  const { container } = render(<Home />);
+  expect(container).toMatchSnapshot();
+});
