4786eb1289c7b878ef49f0d50c6e8ef17525d17f
Sherry He <xsherryhe@gmail.com>
2023-01-12 12:26:25 -0500

    Wrote basic tests for App parent component

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
+import './setupTests';
 import App from '../App';
 
-test('renders learn react link', () => {
+jest.mock('../components/Header', () => {
+  return () => <header>Header</header>;
+});
+
+jest.mock('react-router-dom', () => {
+  return {
+    Routes: () => 'Routes',
+    Route: () => 'Route',
+    useLocation: () => ({ pathname: '' }),
+  };
+});
+
+it('renders the header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = screen.getByRole('banner');
+  expect(header).toBeInTheDocument();
+});
+
+it('renders the main content', () => {
+  render(<App />);
+  const main = screen.getByRole('main');
+  expect(main).toBeInTheDocument();
 });
